b7cd20de4d4ebf7eef703b2a088fad3a138441f0
Andreas Pfohl <andreas@pf0hl.de>
2013-06-28 12:17:18 +0200

    Initial Commit

---
+++ SpaceAPI/SAPIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SAPIAppDelegate.h
+//  SpaceAPI
+//
+//  Created by Andreas Pfohl on 28.06.13.
+//  Copyright (c) 2013 Andreas Pfohl. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SAPIAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ SpaceAPI/SAPIAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  SAPIAppDelegate.m
+//  SpaceAPI
+//
+//  Created by Andreas Pfohl on 28.06.13.
+//  Copyright (c) 2013 Andreas Pfohl. All rights reserved.
+//
+
+#import "SAPIAppDelegate.h"
+
+@implementation SAPIAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+  // Insert code here to initialize your application
+}
+
+@end
